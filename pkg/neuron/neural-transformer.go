f2b7c3f831e4c9f986eda88c03bea5082f0e5d09
Charles Justin Reusnow <Charles.J.Reusnow@centene.com>
2019-01-02 21:40:54 -0600

    Added derivatives

--- pkg/neuron/neural-transformer.go
+++ pkg/neuron/neural-transformer.go
@@ -12,9 +12,29 @@ func sigmoid(x float64) (y float64) {
 }
 
 func rectifiedLinearUnit(x float64) (y float64) {
-	return math.Max(x, util.Midpoint)
+	if x > 0 {
+		return x
+	}
+	return util.Midpoint
 }
 
 func passThrough(x float64) (y float64) {
 	return x
+}
+
+//////////
+
+func dSigmoid(x float64) (y float64) {
+	return sigmoid(x) * (1 - sigmoid(x))
+}
+
+func dRectifiedLinearUnit(x float64) (y float64) {
+	if x > 0 {
+		return 1.0
+	}
+	return 0.0
+}
+
+func dPassThrough(x float64) (y float64) {
+	return 1.0
 }
