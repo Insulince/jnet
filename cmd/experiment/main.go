85ebbb1de940f0afb0de2238934e4afc1d3f9f32
Charles Justin Reusnow <Charles.J.Reusnow@centene.com>
2019-01-20 12:27:37 -0600

    Serialization created. You can now SAVE your pre-trained models in string form, then load them later without having to re-train them. An example neuron that went through 2.5 million training iterations (took about 2 hours to train) is under cmd/string/main.go and is ready to predict at a moment's notice.

--- cmd/experiment/main.go
+++ cmd/experiment/main.go
@@ -12,16 +12,16 @@ func init() {
 
 func main() {
 	nw := jnet.NewNetwork(25, 16, 16, 10)
-	nw.LearningRate = 0.01
-	nw.TrainingIterations = 1000000
-	nw.MiniBatchSize = 30
+	nw.LearningRate = 0.1
+	nw.TrainingIterations = 2500000
+	nw.MiniBatchSize = 32
 
 	nw.Train(pkg.TrainingData)
 
 	hd := jnet.HumanData{
 		Data: [][]float64{
 			{1, 1, 1, 0, 0},
-			{1, 0, 0, 0, 0},
+			{1, 0, 1, 0, 0},
 			{1, 1, 1, 0, 0},
 			{0, 0, 1, 0, 0},
 			{1, 1, 1, 0, 0},
@@ -32,4 +32,7 @@ func main() {
 	nw.Predict(realData)
 
 	fmt.Println(nw.GetResults())
+	fmt.Println()
+	fmt.Println(nw.Serialize())
+	fmt.Println()
 }
--- pkg/jnet/jnet.go
+++ pkg/jnet/jnet.go
@@ -5,6 +5,8 @@ import (
 	"github.com/TheDemx27/calculus"
 	"math"
 	"math/rand"
+	"strconv"
+	"strings"
 )
 
 ///////////////////////// TRAINING DATA /////////////////////////
@@ -91,6 +93,8 @@ func NewNeuron(pl *Layer) (nn *Neuron) {
 
 			nn.Connections = append(nn.Connections, NewConnection(pln, nn))
 		}
+	} else {
+		nn.Bias = 0.0 // This is an input neuron, so set the bias to 0.0 to minimize any confusion when debugging.
 	}
 
 	return nn
@@ -487,6 +491,7 @@ func (nw *Network) Train(trainingData []TrainingData) (this *Network) {
 			c = 0
 		}
 
+		// TODO: Make better
 		if averageAverageLoss < 0.1 {
 			break
 		}
@@ -635,4 +640,239 @@ func (nw *Network) GetResults() (results string) {
 	results = fmt.Sprintf("%vResult: %v\n", results, result)
 
 	return results
+}
+
+func Deserialize(networkString string) (this *Network, err error) {
+	var data [][]float64
+
+	lines := strings.Split(networkString, "\n")
+	ql := len(lines)
+	for li := 0; li < ql; li++ {
+		line := lines[li]
+
+		data = append(data, []float64{})
+
+		items := strings.Split(line, " ")
+		qi := len(items)
+		for ii := 0; ii < qi; ii++ {
+			item := items[ii]
+
+			value, err := strconv.ParseFloat(item, 64)
+			if err != nil {
+				return nil, err
+			}
+			data[li] = append(data[li], value)
+		}
+	}
+
+	lc := 0
+
+	// FIRST LINE
+	fl := data[0]
+
+	if len(fl) != 1 {
+		panic("Invalid version identifier!")
+	}
+
+	_ = fl[0]
+
+	lc++
+
+	// SECOND LINE
+	sl := data[1]
+
+	if len(sl) != 3 {
+		panic("Different number of configuration values provided than expected!")
+	}
+
+	lr := sl[0]
+	ti := sl[1]
+	mbs := sl[2]
+
+	lc++
+
+	// THIRD LINE
+	tl := data[2]
+	qnils := tl
+
+	lc++
+
+	// BIASES LINES
+	var biases [][]float64
+	for i := 1; i < len(qnils); i++ {
+		biases = append(biases, []float64{})
+
+		line := data[lc]
+
+		qn := int(qnils[i])
+		if len(line) != qn {
+			panic("Different number of biases than expected!")
+		}
+
+		bi := i - 1
+		for j := 0; j < qn; j++ {
+			biases[bi] = append(biases[bi], line[j])
+		}
+
+		lc++
+	}
+
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println()
+		}
+	}()
+
+	// WEIGHTS LINES
+	var weights [][][]float64
+	for i := 1; i < len(qnils); i++ {
+		weights = append(weights, [][]float64{})
+
+		qn := int(qnils[i])
+		for j := 0; j < qn; j++ {
+			weights[i-1] = append(weights[i-1], []float64{})
+
+			line := data[lc]
+
+			qpn := int(qnils[i-1])
+			for k := 0; k < qpn; k++ {
+				weights[i-1][j] = append(weights[i-1][j], line[k])
+			}
+
+			lc++
+		}
+	}
+
+	fmt.Printf("%#v\n", lr)
+	fmt.Printf("%#v\n", ti)
+	fmt.Printf("%#v\n", mbs)
+	fmt.Printf("%#v\n", qnils)
+	fmt.Printf("%#v\n", biases)
+	fmt.Printf("%#v\n", weights)
+
+	// CREATE NETWORK
+	nw := &Network{}
+
+	// CONFIGURE
+	nw.LearningRate = lr
+	nw.TrainingIterations = int(ti)
+	nw.MiniBatchSize = int(mbs)
+
+	// BUILD LAYERS & INITIAL CONNECTIONS
+	for i := 0; i < len(qnils); i++ {
+		if i == 0 {
+			nw.Layers = append(nw.Layers, NewLayer(int(qnils[i]), nil))
+		} else {
+			pl := nw.Layers[i-1]
+			nw.Layers = append(nw.Layers, NewLayer(int(qnils[i]), pl))
+		}
+	}
+
+	// SETUP WEIGHTS AND BIASES
+	for li := 1; li < len(nw.Layers); li++ {
+		l := nw.Layers[li]
+
+		// BIASES
+		for ni := 0; ni < len(l.Neurons); ni++ {
+			n := l.Neurons[ni]
+
+			n.Bias = biases[li-1][ni]
+		}
+
+		// WEIGHTS
+		for ni := 0; ni < len(l.Neurons); ni++ {
+			n := l.Neurons[ni]
+
+			for ci := 0; ci < len(n.Connections); ci++ {
+				c := n.Connections[ci]
+
+				c.Weight = weights[li-1][ni][ci]
+			}
+		}
+	}
+
+	return nw, nil
+}
+
+const SerializeVerion = "1.0"
+
+func (nw *Network) Serialize() (networkString string) {
+	/*
+		1.0
+		0.1 2500000 32
+		5 3 4
+		1 2 3
+		2 5 7 2
+		1 1 1 1 1
+		1 1 1 1 1
+		1 1 1 1 1
+		2 2 2
+		2 2 2
+		2 2 2
+		2 2 2
+	*/
+
+	// FIRST LINE
+	networkString = fmt.Sprintf("%v\n", SerializeVerion)
+
+	// SECOND LINE
+	networkString += fmt.Sprintf("%v %v %v\n", nw.LearningRate, nw.TrainingIterations, nw.MiniBatchSize)
+
+	// THIRD LINE
+	ql := len(nw.Layers)
+	for li := 0; li < ql; li++ {
+		l := nw.Layers[li]
+
+		networkString += fmt.Sprintf("%v", len(l.Neurons))
+		if li < ql-1 {
+			networkString += " "
+		} else {
+			networkString += "\n"
+		}
+	}
+
+	// BIASES LINES
+	for li := 1; li < ql; li++ {
+		l := nw.Layers[li]
+
+		qn := len(l.Neurons)
+		for ni := 0; ni < qn; ni++ {
+			n := l.Neurons[ni]
+
+			networkString += fmt.Sprintf("%v", n.Bias)
+
+			if ni < qn-1 {
+				networkString += " "
+			} else {
+				networkString += "\n"
+			}
+		}
+	}
+
+	// WEIGHTS LINES
+	for li := 0; li < ql; li++ {
+		l := nw.Layers[li]
+
+		qn := len(l.Neurons)
+		for ni := 0; ni < qn; ni++ {
+			n := l.Neurons[ni]
+
+			qc := len(n.Connections)
+			for ci := 0; ci < qc; ci++ {
+				c := n.Connections[ci]
+
+				networkString += fmt.Sprintf("%v", c.Weight)
+
+				if ci < qc-1 {
+					networkString += " "
+				} else {
+					if li < ql-1 || ni < qn-1 { // The string should not end with a newline, so if this is the last neuron, don't use a new line.
+						networkString += "\n"
+					}
+				}
+			}
+		}
+	}
+
+	return networkString
 }
